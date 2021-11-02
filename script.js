aee461e9344a48a78f092e836735df2bad00abe0
Muhamind26 <muhammadamindmw@gmail.com>
2021-11-02 22:37:26 +0700

    Website Mini Project v1

---
+++ script.js
@@ -0,0 +1,13 @@
+//style home
+const home = document.getElementById('home')
+home.style.color = '#E26A2C'
+
+// style aboutUs
+const aboutUs = document.getElementById('aboutUs')
+aboutUs.style.color = '#E26A2C'
+
+// Text Banner
+const text = document.querySelector('.text');
+text.addEventListener('mouseover', function () {
+    alert('Get your best deal on TravelManiac')
+})
