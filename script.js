fb1de908780f781e1e84a4784523957c1f6543e0
Muhamind26 <89624758+Muhamind26@users.noreply.github.com>
2022-03-27 21:10:51 +0700

    commit on main

--- script.js
+++ script.js
@@ -5,9 +5,3 @@ home.style.color = '#E26A2C'
 // style aboutUs
 const aboutUs = document.getElementById('aboutUs')
 aboutUs.style.color = '#E26A2C'
-
-// Text Banner
-const text = document.querySelector('.text');
-text.addEventListener('mouseover', function () {
-    alert('Get your best deal on TravelManiac')
-})
